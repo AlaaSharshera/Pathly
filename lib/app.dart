6f194102e5da07e67c2bde318abd82fda9dd0d15
alaa <alaasharshera123@gmail.com>
2025-04-30 12:07:50 +0300

    add dependencies

---
+++ lib/app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Pathly extends StatelessWidget {
+  const Pathly({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(debugShowCheckedModeBanner: false);
+  }
+}
