2586840c5aab3cb454f10094c45e6e218293b9a1
alaa <alaasharshera123@gmail.com>
2025-04-30 12:33:17 +0300

    add screen util to pathly

--- lib/app.dart
+++ lib/app.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class Pathly extends StatelessWidget {
   const Pathly({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(debugShowCheckedModeBanner: false);
+    return ScreenUtilInit(
+      designSize: Size(393, 851),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (_, child) {
+        return MaterialApp(debugShowCheckedModeBanner: false);
+      },
+    );
   }
 }
