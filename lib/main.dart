614399cc073c7a81cc5aaf1d42ec5f93235b29c1
alaa <alaasharshera123@gmail.com>
2025-05-01 15:33:57 +0300

    add api keys file

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:pathly/app.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:pathly/utils/api_keys.dart';
 import 'firebase_options.dart';
 import 'package:flutter_stripe/flutter_stripe.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  Stripe.publishableKey =
-      "pk_test_51RH2bBIgCCjsMlEkgCPZdYr1Mxu25fvapVo09dkY10KhKQw9YQFIpR9lBNWXqTHVctWMunhL1infFeiS9wY4RX6q00JjSbiuro";
+  Stripe.publishableKey = ApiKeys.stripePuplishedKey;
   runApp(const Pathly());
 }
