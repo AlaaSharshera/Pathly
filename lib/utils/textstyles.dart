7af448b9703d01d1cf89ae042682d06771756acb
alaa <alaasharshera123@gmail.com>
2025-05-02 17:42:05 +0300

    add textstyles class

---
+++ lib/utils/textstyles.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+abstract class Styles {
+  static const TextStyle styleGray12 = TextStyle(
+    color: Color(0xff989393),
+    fontSize: 12,
+  );
+
+  static const TextStyle styleWhiteBold16 = TextStyle(
+    fontSize: 22,
+    fontWeight: FontWeight.w600,
+    color: Colors.white,
+  );
+}
