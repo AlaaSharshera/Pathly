ad249883fab5855e6e1bb24b6d48327a748fd63d
alaa <alaasharshera123@gmail.com>
2025-05-03 22:52:55 +0300

    add VerifyCodeService class

--- lib/services/verify_code_service.dart
+++ lib/services/verify_code_service.dart
@@ -4,18 +4,20 @@ import 'package:pathly/constant.dart';
 
 import 'package:pathly/utils/api.dart';
 
-Future<dynamic> verifyCodeService({
-  required String email,
-  required String code,
-}) async {
-  try {
-    var response = await Api().postRequest(
-      url: "$baseUrl/Auth/verify-code",
-      body: {"email": email, "code": code},
-    );
-    log(response.data.toString());
-    return response.data.toString();
-  } catch (e) {
-    throw Exception(e.toString());
+class VerifyCodeService {
+  Future<dynamic> verifyCodeService({
+    required String email,
+    required String code,
+  }) async {
+    try {
+      var response = await Api().postRequest(
+        url: "$baseUrl/Auth/verify-code",
+        body: {"email": email, "code": code},
+      );
+      log(response.data.toString());
+      return response.data.toString();
+    } catch (e) {
+      throw Exception(e.toString());
+    }
   }
 }
