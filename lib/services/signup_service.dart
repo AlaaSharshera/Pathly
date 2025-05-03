2bbc417659196388272138a774cdcfb0bebe1265
alaa <alaasharshera123@gmail.com>
2025-05-03 22:12:56 +0300

    add class signupservice

--- lib/services/signup_service.dart
+++ lib/services/signup_service.dart
@@ -4,24 +4,26 @@ import 'package:pathly/constant.dart';
 import 'package:pathly/models/auth_response_model.dart';
 import 'package:pathly/utils/api.dart';
 
-Future<AuthResponseModel> signupService({
-  required String username,
-  required String email,
-  required String password,
-}) async {
-  try {
-    var response = await Api().postRequest(
-      url: "$baseUrl/Auth/register",
-      body: {
-        "username": username,
-        "email": email,
-        "password": password,
-        "isAdmin": false,
-      },
-    );
-    log(response.data.toString());
-    return AuthResponseModel.fromJson(response.data);
-  } catch (e) {
-    throw Exception(e.toString());
+class SignupService {
+  Future<AuthResponseModel> signupService({
+    required String username,
+    required String email,
+    required String password,
+  }) async {
+    try {
+      var response = await Api().postRequest(
+        url: "$baseUrl/Auth/register",
+        body: {
+          "username": username,
+          "email": email,
+          "password": password,
+          "isAdmin": false,
+        },
+      );
+      log(response.data.toString());
+      return AuthResponseModel.fromJson(response.data);
+    } catch (e) {
+      throw Exception(e.toString());
+    }
   }
 }
