89ff87b54c8f94da0bf0590d99db815314d6c76f
alaa <alaasharshera123@gmail.com>
2025-05-03 22:58:04 +0300

    add ResetPassService class

--- lib/services/reset_pass_service.dart
+++ lib/services/reset_pass_service.dart
@@ -4,24 +4,26 @@ import 'package:pathly/constant.dart';
 
 import 'package:pathly/utils/api.dart';
 
-Future<dynamic> resetPassService({
-  required String email,
-  required String password,
-  required String confirmPassword,
-}) async {
-  try {
-    var response = await Api().postRequest(
-      url: "$baseUrl/Auth/reset-password",
-      body: {
-        "email": email,
+class ResetPassService {
+  Future<dynamic> resetPassService({
+    required String email,
+    required String password,
+    required String confirmPassword,
+  }) async {
+    try {
+      var response = await Api().postRequest(
+        url: "$baseUrl/Auth/reset-password",
+        body: {
+          "email": email,
 
-        "newPassword": password,
-        "confirmPassword": confirmPassword,
-      },
-    );
-    log(response.data.toString());
-    return response.data.toString();
-  } catch (e) {
-    throw Exception(e.toString());
+          "newPassword": password,
+          "confirmPassword": confirmPassword,
+        },
+      );
+      log(response.data.toString());
+      return response.data.toString();
+    } catch (e) {
+      throw Exception(e.toString());
+    }
   }
 }
