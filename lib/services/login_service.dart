e7ff9a995fd57b7639ca7a419886156bc9f8eb19
alaa <alaasharshera123@gmail.com>
2025-05-03 22:13:45 +0300

    add loginservice class

--- lib/services/login_service.dart
+++ lib/services/login_service.dart
@@ -4,18 +4,20 @@ import 'package:pathly/constant.dart';
 import 'package:pathly/models/auth_response_model.dart';
 import 'package:pathly/utils/api.dart';
 
-Future<AuthResponseModel> loginService({
-  required String email,
-  required String password,
-}) async {
-  try {
-    var response = await Api().postRequest(
-      url: "$baseUrl/Auth/login",
-      body: {"email": email, "password": password, "isAdmin": false},
-    );
-    log(response.data.toString());
-    return AuthResponseModel.fromJson(response.data);
-  } catch (e) {
-    throw Exception(e.toString());
+class LoginService {
+  Future<AuthResponseModel> loginService({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      var response = await Api().postRequest(
+        url: "$baseUrl/Auth/login",
+        body: {"email": email, "password": password, "isAdmin": false},
+      );
+      log(response.data.toString());
+      return AuthResponseModel.fromJson(response.data);
+    } catch (e) {
+      throw Exception(e.toString());
+    }
   }
 }
