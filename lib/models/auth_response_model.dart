a67439cf3dfba2c88df2d5bf0fa4297332dc61b4
alaa <alaasharshera123@gmail.com>
2025-05-03 20:46:59 +0300

    rename the model

--- lib/models/auth_response_model.dart
+++ lib/models/auth_response_model.dart
@@ -1,14 +1,14 @@
-class RegisterResponseModel {
+class AuthResponseModel {
   final bool isAuthenticated;
   final String token;
   final String expiresOn;
-  RegisterResponseModel({
+  AuthResponseModel({
     required this.expiresOn,
     required this.isAuthenticated,
     required this.token,
   });
-  factory RegisterResponseModel.fromJson(Map<String, dynamic> json) {
-    return RegisterResponseModel(
+  factory AuthResponseModel.fromJson(Map<String, dynamic> json) {
+    return AuthResponseModel(
       expiresOn: json['expiresOn'],
       isAuthenticated: json['isAuthenticated'],
       token: json['token'],
