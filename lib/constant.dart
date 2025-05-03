4902ee1c40279ee9c34f95fa34f028a760a990ea
alaa <alaasharshera123@gmail.com>
2025-05-03 20:50:50 +0300

    add base url

--- lib/constant.dart
+++ lib/constant.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
 const String kPathlyLogo = 'assets/images/logo.png';
+const baseUrl = "https://pathly.runasp.net/api";
+
 const Color kPrimaryColor = Color(0xff173956);
 const Color kBlackColor = Color(0xff282828);
