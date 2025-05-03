d5bf2f2cffeab6e084b052a59761b2e971366474
alaa <alaasharshera123@gmail.com>
2025-05-03 20:48:55 +0300

    add forget pass service

---
+++ lib/services/forget_pass_service.dart
@@ -0,0 +1,18 @@
+import 'dart:developer';
+
+import 'package:pathly/constant.dart';
+
+import 'package:pathly/utils/api.dart';
+
+Future<dynamic> forgetPassService({required String email}) async {
+  try {
+    var response = await Api().postRequest(
+      url: "$baseUrl/Auth/forget-password",
+      body: {"email": email},
+    );
+    log(response.data.toString());
+    return response.data.toString();
+  } catch (e) {
+    throw Exception(e.toString());
+  }
+}
