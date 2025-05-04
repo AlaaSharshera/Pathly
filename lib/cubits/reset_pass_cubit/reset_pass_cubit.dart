bd0c1e1da09450be6abf91d71b51fdbf77f1a2d8
alaa <alaasharshera123@gmail.com>
2025-05-04 15:30:05 +0300

    add ResetPassCubit

---
+++ lib/cubits/reset_pass_cubit/reset_pass_cubit.dart
@@ -0,0 +1,23 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:pathly/cubits/reset_pass_cubit/reset_pass_states.dart';
+import 'package:pathly/services/reset_pass_service.dart';
+
+class ResetPassCubit extends Cubit<ResetPassStates> {
+  ResetPassCubit() : super(LoadingResetPassState());
+  void resetPass({
+    required String email,
+    required String password,
+    required String confirmPassword,
+  }) async {
+    try {
+      String response = await ResetPassService().resetPassService(
+        email: email,
+        password: password,
+        confirmPassword: confirmPassword,
+      );
+      emit(SuccessResetPassState(response));
+    } catch (e) {
+      emit(FailureResetPassState(e.toString()));
+    }
+  }
+}
