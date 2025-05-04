3e018029893bbe84f65aaa1270c509e43015c2e7
alaa <alaasharshera123@gmail.com>
2025-05-04 21:51:14 +0300

    integrate login cubit

--- lib/widgets/auth_widgets/login_form.dart
+++ lib/widgets/auth_widgets/login_form.dart
@@ -1,7 +1,12 @@
+import 'dart:developer';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:pathly/constant.dart';
+import 'package:pathly/cubits/login_cubit/login_cubit.dart';
+import 'package:pathly/cubits/login_cubit/login_states.dart';
 import 'package:pathly/utils/validators.dart';
 import 'package:pathly/widgets/auth_widgets/auth_button.dart';
 import 'package:pathly/widgets/auth_widgets/auth_center_text.dart';
@@ -29,76 +34,114 @@ class _CustomLoginFormState extends State<CustomLoginForm> {
   Widget build(BuildContext context) {
     return Form(
       key: formKey,
-      child: Column(
-        children: [
-          AuthCenterText(text: "Welcome back!"),
-          AuthlabelTextField(text: "E-mail"),
-          Padding(
-            padding: EdgeInsets.only(bottom: 16.h),
-            child: AuthTextField(
-              hintText: "YourName@example.com",
+      child: BlocListener<LoginCubit, LoginStates>(
+        listener: (context, state) {
+          if (state is LoadingLoginState) {
+            log("loading");
+            Get.dialog(
+              Center(child: CircularProgressIndicator(color: kPrimaryColor)),
+              barrierDismissible: false,
+            );
+          } else if (state is SuccessLoginState) {
+            Get.back();
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                content: Text(
+                  "Login Successfully",
+                  style: GoogleFonts.poppins(
+                    color: kPrimaryColor,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                backgroundColor: Colors.white,
+              ),
+            );
+          } else if (state is FailureLoginState) {
+            Get.back();
+            ScaffoldMessenger.of(
+              context,
+            ).showSnackBar(SnackBar(content: Text(state.errMessage)));
+          }
+        },
+        child: Column(
+          children: [
+            AuthCenterText(text: "Welcome back!"),
+            AuthlabelTextField(text: "E-mail"),
+            Padding(
+              padding: EdgeInsets.only(bottom: 16.h),
+              child: AuthTextField(
+                hintText: "YourName@example.com",
+                autovalidateMode: autovalidateMode,
+                onSaved: (value) {
+                  email = value;
+                },
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "please Enter your E-mail";
+                  } else if (!RegExp(
+                    r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$',
+                  ).hasMatch(value)) {
+                    return "please Enter valid E-mail";
+                  }
+                  return null;
+                },
+              ),
+            ),
+            AuthlabelTextField(text: "Password"),
+            AuthTextField(
+              hintText: "••••••••••••••••••",
               autovalidateMode: autovalidateMode,
+              obscureText: true,
               onSaved: (value) {
-                email = value;
+                pass = value;
               },
               validator: (value) {
                 if (value == null || value.isEmpty) {
-                  return "please Enter your E-mail";
-                } else if (!RegExp(
-                  r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$',
-                ).hasMatch(value)) {
-                  return "please Enter valid E-mail";
+                  return "Please ,Enter Password";
+                } else if (validatePassword(value).isNotEmpty) {
+                  return "pass must have at least ${validatePassword(value)}";
                 }
                 return null;
               },
             ),
-          ),
-          AuthlabelTextField(text: "Password"),
-          AuthTextField(
-            hintText: "••••••••••••••••••",
-            autovalidateMode: autovalidateMode,
-            obscureText: true,
-            onSaved: (value) {
-              pass = value;
-            },
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return "Please ,Enter Password";
-              } else if (validatePassword(value).isNotEmpty) {
-                return "pass must have at least ${validatePassword(value)}";
-              }
-              return null;
-            },
-          ),
-          Padding(
-            padding: EdgeInsets.symmetric(vertical: 8.h),
-            child: ForgetPassRow(),
-          ),
-          AuthButton(
-            buttonText: "Sign In",
-            onPressed: () async {
-              autovalidateMode = AutovalidateMode.onUserInteraction;
-              FocusScope.of(context).unfocus();
-              if (formKey.currentState!.validate()) {
-                formKey.currentState!.save();
-              }
-            },
-          ),
-          Padding(
-            padding: EdgeInsets.symmetric(vertical: 16.h),
-            child: Text(
-              "or",
-              style: GoogleFonts.poppins(
-                textStyle: TextStyle(color: kBlackColor, fontSize: 20.sp),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 8.h),
+              child: ForgetPassRow(),
+            ),
+            AuthButton(
+              buttonText: "Sign In",
+              onPressed: () async {
+                autovalidateMode = AutovalidateMode.onUserInteraction;
+                FocusScope.of(context).unfocus();
+                if (formKey.currentState!.validate()) {
+                  formKey.currentState!.save();
+                  try {
+                    context.read<LoginCubit>().loginService(
+                      email: email!,
+                      password: pass!,
+                    );
+                  } catch (e) {
+                    throw Exception("Cubit failed");
+                  }
+                }
+              },
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 16.h),
+              child: Text(
+                "or",
+                style: GoogleFonts.poppins(
+                  textStyle: TextStyle(color: kBlackColor, fontSize: 20.sp),
+                ),
               ),
             ),
-          ),
-          AuthOptionsRow(
-            onpressedfacebook: () {},
-            onPressedgoogle: () {},
-            onPressedApple: () {},
-          ),
-        ],
+            AuthOptionsRow(
+              onpressedfacebook: () {},
+              onPressedgoogle: () {},
+              onPressedApple: () {},
+            ),
+          ],
+        ),
       ),
     );
   }
