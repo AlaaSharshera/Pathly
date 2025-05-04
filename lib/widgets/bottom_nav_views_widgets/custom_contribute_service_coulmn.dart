ddd2a4e30622f13892868eba93564af7b2b2d0f8
alaa <alaasharshera123@gmail.com>
2025-05-04 22:14:45 +0300

    add CustomContributeServiceCoulmn

---
+++ lib/widgets/bottom_nav_views_widgets/custom_contribute_service_coulmn.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:pathly/constant.dart';
+import 'package:pathly/models/custom_contribute_model.dart';
+
+class CustomContributeServiceCoulmn extends StatelessWidget {
+  const CustomContributeServiceCoulmn({
+    required this.contributeModel,
+    super.key,
+  });
+  final CustomContributeModel contributeModel;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SvgPicture.asset(contributeModel.image),
+        Text(
+          contributeModel.text,
+          style: GoogleFonts.poppins(color: kPrimaryColor, fontSize: 13),
+        ),
+      ],
+    );
+  }
+}
