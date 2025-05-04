501944baa8d2517f5df5fd5f3965b54a37864e8a
alaa <alaasharshera123@gmail.com>
2025-05-04 22:22:13 +0300

    add CustomProfileDivider

---
+++ lib/widgets/bottom_nav_views_widgets/custom_profile_divider.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CustomProfileDivider extends StatelessWidget {
+  const CustomProfileDivider({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Divider(color: Color(0xffD1D1D6), indent: 25, endIndent: 25);
+  }
+}
