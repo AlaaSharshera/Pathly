d4cf4c819ac8c994089e385305923548b78e0ff1
alaa <alaasharshera123@gmail.com>
2025-05-08 21:26:04 +0300

    add services

--- lib/utils/api.dart
+++ lib/utils/api.dart
@@ -5,6 +5,67 @@ import 'package:dio/dio.dart';
 class Api {
   Dio dio = Dio();
 
+  Future<Response> postStripe({
+    required String url,
+    required String token,
+    required body,
+    String? contentType,
+  }) async {
+    var response = await dio.post(
+      url,
+      data: body,
+      options: Options(
+        contentType: contentType,
+        headers: {'Authorization': 'Bearer $token'},
+      ),
+    );
+    return response;
+  }
+
+  Future<dynamic> get({required String url}) async {
+    try {
+      var response = await dio.get(url);
+      if (response.statusCode == 200) {
+        return response.data;
+      } else {
+        throw Exception(
+          'Failed to load subscription plans statusCode is:${response.statusCode}',
+        );
+      }
+    } on DioException catch (e) {
+      var errMessage = e.response?.data ?? e.message;
+      throw Exception(errMessage.toString());
+    }
+  }
+
+  Future<dynamic> post({
+    required String url,
+    required String token,
+    required body,
+    String? contentType,
+  }) async {
+    try {
+      var response = await dio.post(
+        url,
+        data: body,
+        options: Options(
+          contentType: contentType,
+          headers: {'Authorization': 'Bearer $token'},
+        ),
+      );
+      if (response.statusCode == 200) {
+        return response.data;
+      } else {
+        throw Exception(
+          'Failed to load subscription plans statusCode is:${response.statusCode}',
+        );
+      }
+    } on DioException catch (e) {
+      var errMessage = e.response?.data ?? e.message;
+      throw Exception(errMessage.toString());
+    }
+  }
+
   Future<Response> postRequest({
     required String url,
     required body,
