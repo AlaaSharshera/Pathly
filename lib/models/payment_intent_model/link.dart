1029979db359e9bc812125a2d00bf3644f367d24
alaa <alaasharshera123@gmail.com>
2025-05-14 12:02:05 +0300

    some edits

--- lib/models/payment_intent_model/link.dart
+++ lib/models/payment_intent_model/link.dart
@@ -3,11 +3,8 @@ class Link {
 
   Link({this.persistentToken});
 
-  factory Link.fromJson(Map<String, dynamic> json) => Link(
-        persistentToken: json['persistent_token'] as dynamic,
-      );
+  factory Link.fromJson(Map<String, dynamic> json) =>
+      Link(persistentToken: json['persistent_token'] as dynamic);
 
-  Map<String, dynamic> toJson() => {
-        'persistent_token': persistentToken,
-      };
+  Map<String, dynamic> toJson() => {'persistent_token': persistentToken};
 }
--- lib/models/payment_intent_model/payment_method_options.dart
+++ lib/models/payment_intent_model/payment_method_options.dart
@@ -9,17 +9,19 @@ class PaymentMethodOptions {
 
   factory PaymentMethodOptions.fromJson(Map<String, dynamic> json) {
     return PaymentMethodOptions(
-      card: json['card'] == null
-          ? null
-          : Card.fromJson(json['card'] as Map<String, dynamic>),
-      link: json['link'] == null
-          ? null
-          : Link.fromJson(json['link'] as Map<String, dynamic>),
+      card:
+          json['card'] == null
+              ? null
+              : Card.fromJson(json['card'] as Map<String, dynamic>),
+      link:
+          json['link'] == null
+              ? null
+              : Link.fromJson(json['link'] as Map<String, dynamic>),
     );
   }
 
   Map<String, dynamic> toJson() => {
-        'card': card?.toJson(),
-        'link': link?.toJson(),
-      };
+    'card': card?.toJson(),
+    'link': link?.toJson(),
+  };
 }
