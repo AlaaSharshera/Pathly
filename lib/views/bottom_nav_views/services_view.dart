4100abb982454ba12ff0a73e6884378be4ebae7f
alaa <alaasharshera123@gmail.com>
2025-05-15 18:02:28 +0300

    provide GetNearbyPlacesCubit

--- lib/views/bottom_nav_views/services_view.dart
+++ lib/views/bottom_nav_views/services_view.dart
@@ -1,25 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:pathly/constant.dart';
+import 'package:pathly/cubits/get_nearby_places_cubit/get_nearby_places_cubit.dart';
+import 'package:pathly/models/services_model.dart';
+import 'package:pathly/views/bottom_nav_views/nearby_places_view.dart';
 import 'package:pathly/widgets/bottom_nav_views_widgets/custom_services_container.dart';
 
 class ServicesView extends StatelessWidget {
   const ServicesView({super.key});
-  final List<String> imagesCoulmn1 = const [
-    "assets/images/must_eat.png",
-    "assets/images/car_services.png",
-    "assets/images/cafe.png",
-    "assets/images/shopping.png",
-    "assets/images/beauty.png",
-  ];
-
-  final List<String> imagesCoulmn2 = const [
-    "assets/images/best_hotel.png",
-    "assets/images/historical_places.png",
-    "assets/images/hospital.png",
-    "assets/images/gym.png",
-    "assets/images/pharmacy.png",
-  ];
 
   @override
   Widget build(BuildContext context) {
@@ -62,12 +52,32 @@ class ServicesView extends StatelessWidget {
                         children: [
                           Column(
                             children: List.generate(
-                              imagesCoulmn1.length,
-                              (index) => Padding(
-                                padding: const EdgeInsets.only(bottom: 16),
-                                child: Image.asset(
-                                  imagesCoulmn1[index],
-                                  width: 170,
+                              servicesCoulmn1.length,
+                              (index) => InkWell(
+                                onTap: () {
+                                  Get.to(
+                                    () => BlocProvider<GetNearbyPlacesCubit>(
+                                      create:
+                                          (context) =>
+                                              GetNearbyPlacesCubit()
+                                                ..getNearbyPlaces(
+                                                  lat: 31.4355529,
+                                                  lng: 31.6746909,
+                                                  type:
+                                                      servicesCoulmn1[index]
+                                                          .serviceName,
+                                                ),
+                                      child: NearbyPlacesView(serviceName:servicesCoulmn1[index]
+                                                          .serviceName ,),
+                                    ),
+                                  );
+                                },
+                                child: Padding(
+                                  padding: const EdgeInsets.only(bottom: 16),
+                                  child: Image.asset(
+                                    servicesCoulmn1[index].image,
+                                    width: 170,
+                                  ),
                                 ),
                               ),
                             ),
@@ -77,12 +87,34 @@ class ServicesView extends StatelessWidget {
                             children: [
                               Column(
                                 children: List.generate(
-                                  imagesCoulmn2.length,
-                                  (index) => Padding(
-                                    padding: const EdgeInsets.only(bottom: 16),
-                                    child: Image.asset(
-                                      imagesCoulmn2[index],
-                                      width: 170,
+                                  servicesCoulmn2.length,
+                                  (index) => InkWell(
+                                    onTap: () {
+                                       Get.to(
+                                    () => BlocProvider<GetNearbyPlacesCubit>(
+                                      create:
+                                          (context) =>
+                                              GetNearbyPlacesCubit()
+                                                ..getNearbyPlaces(
+                                                  lat: 31.4355529,
+                                                  lng: 31.6746909,
+                                                  type:
+                                                      servicesCoulmn2[index]
+                                                          .serviceName,
+                                                ),
+                                      child: NearbyPlacesView(serviceName: servicesCoulmn2[index]
+                                                          .serviceName,),
+                                    ),
+                                  );
+                                    },
+                                    child: Padding(
+                                      padding: const EdgeInsets.only(
+                                        bottom: 16,
+                                      ),
+                                      child: Image.asset(
+                                        servicesCoulmn2[index].image,
+                                        width: 170,
+                                      ),
                                     ),
                                   ),
                                 ),
@@ -106,7 +138,7 @@ class ServicesView extends StatelessWidget {
                           ),
                         ],
                       ),
-                      SizedBox(height: 100),
+                    
                     ],
                   ),
                 ),
