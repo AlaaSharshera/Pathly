35dbd836f0e9e0f18993a1663f645af562cfdb17
alaa <alaasharshera123@gmail.com>
2025-05-29 15:42:11 +0300

    edit GetNearbyPlacesService

--- lib/services/get_nearbyplaces_service.dart
+++ lib/services/get_nearbyplaces_service.dart
@@ -5,16 +5,18 @@ import 'package:pathly/models/nearby_places_model/nearby_places_model.dart';
 import 'package:pathly/utils/api.dart';
 
 class GetNearbyPlacesService {
-  Future<List<NearbyPlacesModel>> getNearbyPlaces({required double lat,required double lng,required String type}) async {
+  Future<List<NearbyPlacesModel>> getNearbyPlaces({
+    required double lat,
+    required double lng,
+    required String type,
+  }) async {
     try {
       List<dynamic> data = await Api().get(
-        url:
-            "$baseUrl/Places/nearby?lat=$lat&lng=$lng&radius=1000&type=$type",
+        url: "$baseUrl/Places/nearby?lat=$lat&lng=$lng&radius=1000&type=$type",
       );
-      List<NearbyPlacesModel> nearbyPlaces=[];
-      for(int i=0;i<data.length;i++){
-nearbyPlaces.add(NearbyPlacesModel.fromJson(data[i]));
-
+      List<NearbyPlacesModel> nearbyPlaces = [];
+      for (int i = 0; i < data.length; i++) {
+        nearbyPlaces.add(NearbyPlacesModel.fromJson(data[i]));
       }
       log(nearbyPlaces[0].name.toString());
       return nearbyPlaces;
