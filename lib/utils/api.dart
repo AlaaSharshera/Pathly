0538c81bd62322869b8a307445be5bb3f0802464
alaa <alaasharshera123@gmail.com>
2025-06-09 18:58:42 +0300

    add checkInternetConnection method

--- lib/utils/api.dart
+++ lib/utils/api.dart
@@ -1,25 +1,32 @@
 import 'dart:developer';
+import 'dart:io';
 
 import 'package:dio/dio.dart';
 
 class Api {
   Dio dio = Dio();
-   Future<dynamic> get({required String url}) async {
+  Future<bool> checkInternetConnection() async {
+    try {
+      final result = await InternetAddress.lookup('google.com');
+      return result.isNotEmpty && result[0].rawAddress.isNotEmpty;
+    } on SocketException catch (_) {
+      return false;
+    }
+  }
+
+  Future<dynamic> get({required String url}) async {
     try {
       var response = await dio.get(url);
       if (response.statusCode == 200) {
         return response.data;
       } else {
-        throw Exception(
-          'Failed to load subscription plans statusCode is:${response.statusCode}',
-        );
+        throw Exception('there is an error:${response.statusCode}');
       }
     } on DioException catch (e) {
       var errMessage = e.response?.data ?? e.message;
       throw Exception(errMessage.toString());
     }
   }
-
 
   Future<Response> postStripe({
     required String url,
@@ -38,13 +45,17 @@ class Api {
     return response;
   }
 
- 
   Future<dynamic> post({
     required String url,
     required String token,
     required body,
     String? contentType,
   }) async {
+    final hasConnection = await checkInternetConnection();
+    if (!hasConnection) {
+      throw Exception('لا يوجد اتصال بالإنترنت');
+    }
+
     try {
       var response = await dio.post(
         url,
@@ -54,16 +65,22 @@ class Api {
           headers: {'Authorization': 'Bearer $token'},
         ),
       );
+      log(response.statusCode.toString());
       if (response.statusCode == 200) {
         return response.data;
       } else {
         throw Exception(
-          'Failed to load subscription plans statusCode is:${response.statusCode}',
+          'Failed to load data statusCode is:${response.statusCode}',
         );
       }
     } on DioException catch (e) {
-      var errMessage = e.response?.data ?? e.message;
-      throw Exception(errMessage.toString());
+      if (e.type == DioExceptionType.connectionError) {
+        throw Exception('تعذر الاتصال بالخادم. يرجى التحقق من اتصال الإنترنت');
+      } else if (e.type == DioExceptionType.connectionTimeout) {
+        throw Exception('انتهت مهلة الاتصال. يرجى المحاولة مرة أخرى');
+      } else {
+        throw Exception('حدث خطأ غير متوقع: ${e.message}');
+      }
     }
   }
 
@@ -82,7 +99,7 @@ class Api {
       if (response.statusCode == 200) {
         return response;
       } else {
-        throw Exception("erroe ${response.statusCode}");
+        throw Exception("error ${response.statusCode}");
       }
     } on DioException catch (e) {
       var errMessage = e.response?.data ?? e.message;
