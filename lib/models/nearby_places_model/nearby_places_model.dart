56324fb2cacff5acd16af6e9149a3edf1f12ea39
alaa <alaasharshera123@gmail.com>
2025-06-11 01:15:04 +0300

    add NearbyPlacesModel

--- lib/models/nearby_places_model/nearby_places_model.dart
+++ lib/models/nearby_places_model/nearby_places_model.dart
@@ -5,24 +5,34 @@ class NearbyPlacesModel {
   String? address;
   double? rating;
   Location? location;
+  String? photo;
 
-  NearbyPlacesModel({this.name, this.address, this.rating, this.location});
+  NearbyPlacesModel({
+    this.name,
+    this.address,
+    this.rating,
+    this.location,
+    this.photo,
+  });
 
-  factory NearbyPlacesModel.fromJson(Map<String, dynamic> json) =>
-      NearbyPlacesModel(
-        name: json['name'] as String?,
-        address: json['address'] as String?,
-        rating: (json['rating'] as num?)?.toDouble(),
-        location:
-            json['location'] == null
-                ? null
-                : Location.fromJson(json['location'] as Map<String, dynamic>),
-      );
+  factory NearbyPlacesModel.fromJson(Map<String, dynamic> json) {
+    return NearbyPlacesModel(
+      name: json['Name'] as String?,
+      address: json['Address'] as String?,
+      rating: (json['Rating'] as num?)?.toDouble(),
+      location:
+          json['Location'] == null
+              ? null
+              : Location.fromJson(json['Location'] as Map<String, dynamic>),
+      photo: json['Photo'] as String?,
+    );
+  }
 
   Map<String, dynamic> toJson() => {
-    'name': name,
-    'address': address,
-    'rating': rating,
-    'location': location?.toJson(),
+    'Name': name,
+    'Address': address,
+    'Rating': rating,
+    'Location': location?.toJson(),
+    'Photo': photo,
   };
 }
