0acb2a01c3a9802c662104a39d108f4cd0dc90dd
alaa <alaasharshera123@gmail.com>
2025-06-21 01:27:39 +0300

    get color function

---
+++ lib/utils/get_color.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+Color getColorFromString(String colorString) {
+    switch (colorString.toLowerCase()) {
+      case 'green':
+        return Colors.green;
+      case 'orange':
+        return Colors.orange;
+      case 'red':
+        return Colors.red;
+      case 'yellow':
+        return Colors.yellow;
+      default:
+        return Colors.blue; 
+    }
+  }
