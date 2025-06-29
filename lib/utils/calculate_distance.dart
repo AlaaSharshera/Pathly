e2e27447b8b8d1f76ccea79e8a86d668ffb332cb
alaa <alaasharshera123@gmail.com>
2025-06-29 08:21:47 +0300

    add calculateDistance function

---
+++ lib/utils/calculate_distance.dart
@@ -0,0 +1,13 @@
+  import 'dart:math' as math;
+
+double calculateDistance(double lat1, double lon1, double lat2, double lon2) {
+    const double p = 0.017453292519943295;
+    final double a =
+        0.5 -
+        math.cos((lat2 - lat1) * p) / 2 +
+        math.cos(lat1 * p) *
+            math.cos(lat2 * p) *
+            (1 - math.cos((lon2 - lon1) * p)) /
+            2;
+    return 12742 * math.asin(math.sqrt(a));
+  }
