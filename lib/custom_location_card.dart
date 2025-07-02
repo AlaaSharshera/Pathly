b1d57e4e9b31a16c22067fa619882730c6969793
alaa <alaasharshera123@gmail.com>
2025-07-02 06:01:27 +0300

    some edits

--- lib/custom_location_card.dart
+++ lib/custom_location_card.dart
@@ -3,32 +3,44 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:pathly/utils/textstyles.dart';
 
 class CustomLocationCard extends StatelessWidget {
-  const CustomLocationCard({required this.distinationName,super.key});
- final String distinationName;
+  const CustomLocationCard({required this.distinationName, super.key});
+  final String distinationName;
   @override
   Widget build(BuildContext context) {
-    return  Container(
-            margin: EdgeInsets.all(32),
-            padding:EdgeInsets.all(8) ,
-            width: MediaQuery.of(context).size.width,
-            height: 92,
-            decoration: BoxDecoration(color: Colors.white,borderRadius: BorderRadius.circular(16),border:Border.all(color: Color(0xff999999),width: 1)),
-child: Row(children: [
-  Image.asset('assets/icons/origin_to_distination.png'),
-  Expanded(
-   
-    child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-      Text("Your location",style: Styles.stylePrimaryColorbold18),
-      Divider(color: Color(0xff999999),thickness: 1,height: 10,),
-      Text(distinationName,style: Styles.styledarkgrey18,textAlign: TextAlign.start,)
-    ],),
-  ),
-  SvgPicture.asset('assets/icons/arrow_back.svg',height: 35,)
-
-],),
-          );
+    return Container(
+      margin: EdgeInsets.all(32),
+      padding: EdgeInsets.all(12),
+      width: MediaQuery.of(context).size.width,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(16),
+        border: Border.all(color: Color(0xff999999), width: 1),
+      ),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset('assets/icons/origin_to_distination.png'),
+          Expanded(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("Your location", style: Styles.stylePrimaryColorbold16),
+                Divider(color: Color(0xff999999), thickness: 1, height: 10),
+                Text(
+                  distinationName,
+                  style: Styles.styledarkgrey16,
+                  textAlign: TextAlign.start,
+                  maxLines: 3,
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ],
+            ),
+          ),
+          SvgPicture.asset('assets/icons/arrow_back.svg', height: 35),
+        ],
+      ),
+    );
   }
 }
