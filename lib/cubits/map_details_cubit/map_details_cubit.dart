c267bf42e9f93152482e29278cbcd8de71ea181b
alaa <alaasharshera123@gmail.com>
2025-07-03 07:54:22 +0300

    some edits

--- lib/cubits/map_details_cubit/map_details_cubit.dart
+++ lib/cubits/map_details_cubit/map_details_cubit.dart
@@ -1,15 +1,16 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class MapDetailsCubit extends Cubit<String>{
-  MapDetailsCubit():super("");
-  String mapDetails="Road Condition";
+class MapDetailsCubit extends Cubit<String> {
+  MapDetailsCubit() : super("Road Condition");
+  String mapDetails = "Road Condition";
 
-  void trafficCase(){
-    mapDetails="Traffic";
+  void trafficCase() {
+    mapDetails = "Traffic";
     emit(mapDetails);
   }
-   void roadConditionCase(){
-    mapDetails="Road Condition";
+
+  void roadConditionCase() {
+    mapDetails = "Road Condition";
     emit(mapDetails);
   }
 }
