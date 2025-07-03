35375005214d25d6d037ec69cafc30199a726649
alaa <alaasharshera123@gmail.com>
2025-07-03 11:28:05 +0300

    some edits

--- lib/widgets/sliding_panel_button.dart
+++ lib/widgets/sliding_panel_button.dart
@@ -4,26 +4,38 @@ import 'package:pathly/models/slidingpanel_button_model.dart';
 import 'package:pathly/utils/textstyles.dart';
 
 class SlidingPanelButton extends StatelessWidget {
-  const SlidingPanelButton({required this.slidingPanelButtonModel,this.onPressed,super.key});
-final SlidingPanelButtonModel slidingPanelButtonModel;
-final VoidCallback? onPressed;
+  const SlidingPanelButton({
+    required this.slidingPanelButtonModel,
+    this.onPressed,
+    super.key,
+  });
+  final SlidingPanelButtonModel slidingPanelButtonModel;
+  final VoidCallback? onPressed;
   @override
   Widget build(BuildContext context) {
-    return  ElevatedButton(
-                             style: ElevatedButton.styleFrom(
-                              backgroundColor: kPrimaryColor,
-                              disabledBackgroundColor: Color(0xff81A6B5),
-                              fixedSize: Size(slidingPanelButtonModel.width, 48)
-                              ),
-    
-                  onPressed: onPressed, child: Row(
-                              children: [
-                                  Padding(
-                         padding: const EdgeInsets.only(right: 6.0),
-                                    child: slidingPanelButtonModel.icon
-                                  ),
-                                Text(slidingPanelButtonModel.buttonText,style:Styles.styleWhite16),
-                              ],
-                            ));
+    return Padding(
+      padding: const EdgeInsets.only(left: 12.0, bottom: 16),
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: kPrimaryColor,
+          disabledBackgroundColor: Color(0xff81A6B5),
+          fixedSize: Size(slidingPanelButtonModel.width, 48),
+        ),
+
+        onPressed: onPressed,
+        child: Row(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(right: 6.0),
+              child: slidingPanelButtonModel.icon,
+            ),
+            Text(
+              slidingPanelButtonModel.buttonText,
+              style: Styles.styleWhite16,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
