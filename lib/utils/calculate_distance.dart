34ecb9e8b6fdaf4c1eaedc82414cefb6abb396e2
alaa <alaasharshera123@gmail.com>
2025-07-06 10:25:46 +0300

    edit calculateDistance method

--- lib/utils/calculate_distance.dart
+++ lib/utils/calculate_distance.dart
@@ -1,20 +1,8 @@
-  import 'dart:math' as math;
 
-// double calculateDistance(double lat1, double lon1, double lat2, double lon2) {
-//     const double p = 0.017453292519943295;
-//     final double a =
-//         0.5 -
-//         math.cos((lat2 - lat1) * p) / 2 +
-//         math.cos(lat1 * p) *
-//             math.cos(lat2 * p) *
-//             (1 - math.cos((lon2 - lon1) * p)) /
-//             2;
-//     return 12742 * math.asin(math.sqrt(a));
-//   }
 import 'dart:math';
 
 double calculateDistance(double lat1, double lon1, double lat2, double lon2) {
-  const double earthRadius = 6371000; // meters
+  const double earthRadius = 6371000; 
   double dLat = _toRadians(lat2 - lat1);
   double dLon = _toRadians(lon2 - lon1);
   double a = sin(dLat / 2) * sin(dLat / 2) +
