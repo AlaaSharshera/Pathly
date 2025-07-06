1edff1df1521f57762e9b169720065d527989751
alaa <alaasharshera123@gmail.com>
2025-07-06 10:29:42 +0300

    some edits

--- lib/widgets/bottom_nav_views_widgets/custom_contribute_search_textfield.dart
+++ lib/widgets/bottom_nav_views_widgets/custom_contribute_search_textfield.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:pathly/constant.dart';
@@ -7,41 +8,53 @@ class CustomContributeSearchTextField extends StatelessWidget {
   const CustomContributeSearchTextField({
     required this.color,
     required this.onTap,
+    required this.voiceOnTap,
     super.key,
   });
   final Color color;
   final VoidCallback onTap;
+  final VoidCallback voiceOnTap;
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onTap,
-      child: Container(
-        width: 290,
-        height: 45,
-        padding: EdgeInsets.symmetric(vertical: 8),
-        decoration: BoxDecoration(
-          color: color,
-          borderRadius: BorderRadius.circular(16),
-        ),
-        child: Row(
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 12.0),
-              child: SvgPicture.asset("assets/icons/search_icon.svg"),
+    return Stack(
+      children: [
+        GestureDetector(
+          onTap: onTap,
+          child: Container(
+            width: 290,
+            height: 45,
+            padding: EdgeInsets.symmetric(vertical: 8),
+            decoration: BoxDecoration(
+              color: color,
+              borderRadius: BorderRadius.circular(16),
             ),
-            Text("Where to go?", style: Styles.stylegrey16),
-            Spacer(),
-            Padding(
-              padding: const EdgeInsets.only(right: 12.0),
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                  child: SvgPicture.asset("assets/icons/search_icon.svg"),
+                ),
+                Text("Where to go?", style: Styles.stylegrey16),
+              
+              ],
+            ),
+          ),
+        ),
+          Positioned(
+            right: 12,
+            top: 0,
+            bottom: 0,
+            child: GestureDetector(
+              onTap: voiceOnTap,
               child: SvgPicture.asset(
                 "assets/icons/voice.svg",
                 color: kPrimaryColor,
               ),
             ),
-          ],
-        ),
-      ),
+          ),
+      ],
     );
   }
 }
